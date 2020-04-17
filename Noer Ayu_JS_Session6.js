7c2e542df0d24d63086374524a6232de3ad3c408
noerrayu <ayu.intan117@gmail.com>
2020-04-17 14:07:58 +0700

    operasi matematika

---
+++ Noer Ayu_JS_Session6.js
@@ -0,0 +1,31 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function substraction(a, b) {
+    return a - b;
+}
+
+function devide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
+
+
+let tambah = addition(10, 2);
+console.log("Hasil pertambahan = " + tambah);
+let kurang = substraction(10, 2);
+console.log("Hasil pengurangan = " + kurang);
+let bagi = devide(10, 2);
+console.log("Hasil pembagian = " + bagi);
+let kali = multiply(10, 2);
+console.log("Hasil perkalian = " + kali);
+let sisa = modulus(10, 2);
+console.log("Hasil sisa pembagian = " + sisa);
